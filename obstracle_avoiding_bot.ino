23d001b4ec1ae947179b834e5092275c1e04ed94
sai krishna varma <saighost9999@gmail.com>
2019-06-12 12:28:26 +0530

    intial commit

---
+++ obstracle_avoiding_bot.ino
@@ -0,0 +1,40 @@
+  const int trigpin=9;
+const int echopin=10;
+long duration;
+int distance;
+
+void setup()
+{
+  pinMode(trigpin,OUTPUT);
+  pinMode(echopin,INPUT);
+  Serial.begin(9600);
+  pinMode(11,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  
+}
+ void loop()
+ {
+   digitalWrite(trigpin, LOW);
+   delayMicroseconds(2);
+   digitalWrite(trigpin, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(trigpin, LOW);
+   duration=pulseIn(echopin,HIGH);
+   distance=duration*0.034/2;
+   Serial.print("distance");
+   Serial.println(distance);
+   if(distance<30)
+   { digitalWrite(11,HIGH);
+     digitalWrite(12,LOW);
+     digitalWrite(2,LOW);
+     digitalWrite(3,LOW);
+    }  
+   else
+  { digitalWrite(11,HIGH);
+    digitalWrite(12,LOW);
+    digitalWrite(2,HIGH);
+    digitalWrite(3,LOW);
+  } 
+}
---
+++ srkr_motor_fr_br_lf_rg.ino
@@ -0,0 +1,56 @@
+void setup() 
+{
+pinMode(9,OUTPUT);
+pinMode(8,OUTPUT);
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);  
+}
+void loop () 
+{  
+  digitalWrite(9,HIGH);
+  digitalWrite(8,LOW);
+  digitalWrite(2,HIGH);
+  digitalWrite(3,LOW);
+  delay(5000); 
+  digitalWrite(9,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  delay(5000);
+  
+  
+  digitalWrite(9,HIGH);
+  digitalWrite(8,LOW);
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  delay(5000);
+  digitalWrite(8,LOW);
+  digitalWrite(9,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(2,LOW);
+  delay(5000);
+  
+  
+  digitalWrite(9,LOW);
+  digitalWrite(8,HIGH); 
+  digitalWrite(2,LOW);
+  digitalWrite(3,HIGH);
+  delay(5000);
+  digitalWrite(9,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  delay(5000);
+  
+  digitalWrite(9,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(2,HIGH);
+  digitalWrite(3,LOW);
+  delay(5000);
+  digitalWrite(9,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  delay(5000);
+  
+}
